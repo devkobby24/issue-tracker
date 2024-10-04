0d1d6b5b3e127e3d3aa329c9a6947b485b106851
devkobby24 <nanakobby2002@gmail.com>
2024-10-04 17:56:18 +0000

    Build the new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,15 @@
+import { Button, TextArea, TextField } from '@radix-ui/themes'
+import React from 'react'
+
+
+const NewIssuePage = () => {
+  return (
+    <div className='max-w-xl space-y-3'>
+        <TextField.Root placeholder='Create an Issue' />  
+        <TextArea placeholder="Description" />
+        <Button>Submit New Issue</Button>
+    </div>
+  )
+}
+
+export default NewIssuePage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,14 @@
+'use client';
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 const IssuesPage = () => {
   return (
     <div>
-      <Button>New Issue</Button>
+      <Button>
+        <Link href={'/issues/new'}>New</Link>
+      </Button>
     </div>
   )
 }
