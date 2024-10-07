1b5a46e3731a836c29469046bcee574e89770335
devkobby24 <nanakobby2002@gmail.com>
2024-10-07 13:01:49 +0000

    implemented client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button, Callout, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
@@ -7,16 +7,18 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import {z} from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 
 const NewIssuePage = () => {
   const router = useRouter()
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit, formState: {errors} } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   // console.log(register('title'));
   const [error, setError] = useState('');
 
@@ -44,6 +46,7 @@ const NewIssuePage = () => {
         }
       })}>
         <TextField.Root placeholder='Create an Issue' {...register('title')} />
+        {errors.title && <Text color="red" as='p'>{errors.title.message} </Text>}
         <Controller
           name="description"
           control={control}
@@ -51,6 +54,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           }
         />
+        {errors.description && <Text color="red" as='p'>{errors.description.message} </Text>}
 
         <Button>Submit New Issue</Button>
       </form>
