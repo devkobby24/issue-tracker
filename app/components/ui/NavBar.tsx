aaad70d12380db95e19ac8602b7656763d120a99
devkobby24 <nanakobby2002@gmail.com>
2024-11-09 18:10:36 +0000

    Refactor NavBar component: restructure list items for better semantic HTML and improve accessibility

--- app/components/ui/NavBar.tsx
+++ app/components/ui/NavBar.tsx
@@ -21,25 +21,28 @@ const NavBar = () => {
   return (
     <nav className="flex px-2 md:px-4 font-sans h-14 items-center justify-between shadow-md fixed z-50 bg-white w-full">
       <ul className="flex space-x-2 justify-center items-center">
-        <Link href="/" aria-label="Home">
-          <AiFillBug size={25} />
-        </Link>
-        {links.map(({ href, label }) => (
-          <Link
-            key={href}
-            className={classNames(
-              "md:text-xl text-sm font-semibold md:p-2 rounded-md p-1",
-              {
-                "text-zinc-900 border border-gray-300 shadow-md":
-                  currentPath === href,
-                "text-zinc-500": currentPath !== href,
-                "hover:text-zinc-900 transition-colors": true,
-              }
-            )}
-            href={href}
-          >
-            <li>{label}</li>
+        <li>
+          <Link href="/" aria-label="Home">
+            <AiFillBug size={25} />
           </Link>
+        </li>
+        {links.map(({ href, label }) => (
+          <li key={href}>
+            <Link
+              className={classNames(
+                "md:text-xl text-sm font-semibold md:p-2 rounded-md p-1",
+                {
+                  "text-zinc-900 border border-gray-300 shadow-md":
+                    currentPath === href,
+                  "text-zinc-500": currentPath !== href,
+                  "hover:text-zinc-900 transition-colors": true,
+                }
+              )}
+              href={href}
+            >
+              {label}
+            </Link>
+          </li>
         ))}
       </ul>
 
